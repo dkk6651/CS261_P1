e0259414204cd64611d996d63bffccd93e0bb849
Daniel <dkk6651@rit.edu>
2022-01-20 23:25:58 +0900

    Finished implementing Stable.java but added no comments Works as intended and required with no input check

--- src/Stable.java
+++ src/Stable.java
@@ -21,11 +21,13 @@ public class Stable {
         women = new ArrayList<String>();
         menRanking = new HashMap<String, List<String>>();
         womenRanking = new HashMap<String, List<String>>();
+        Map<String, String> matches = null;
+        int count = 0;
+        StringBuilder temp = new StringBuilder();
 
         try{
             String line = null;
             String[] lineArray = null;
-            int count;
 
             fileReader = new BufferedReader(new FileReader(input));
             while((line = fileReader.readLine()) != null){
@@ -56,21 +58,58 @@ public class Stable {
                 if(fileReader != null){
                     fileReader.close();
                 }
-                System.out.println(men);
-                System.out.println(women);
-                for(String k : men){
-                    System.out.println(menRanking.get(k));
-                }
-                for(String k : women){
-                    System.out.println(womenRanking.get(k));
-                }
-                Map<String, String> matches = matching();
-                for(Map.Entry<String, String> matching : matches.entrySet()){
-                    System.out.println(matching.getKey() + " " + matching.getValue());
-                }
+//                System.out.println(men);
+//                System.out.println(women);
+//                for(String k : men){
+//                    System.out.println(menRanking.get(k));
+//                }
+//                for(String k : women){
+//                    System.out.println(womenRanking.get(k));
+//                }
+                matches = matching();
+//                for(Map.Entry<String, String> matching : matches.entrySet()){
+//                    System.out.println(matching.getKey() + " " + matching.getValue());
+//                }
             } catch (IOException e){
                 e.printStackTrace();
             }
+
+            temp.append(count).append('\n');
+
+            for(String m : men){
+                temp.append(m);
+                for(String i : menRanking.get(m)){
+                    temp.append(' ').append(i);
+                }
+                temp.append('\n');
+            }
+
+            for(String w : women){
+                temp.append(w);
+                for(String i : womenRanking.get(w)){
+                    temp.append(' ').append(i);
+                }
+                temp.append('\n');
+            }
+
+            assert matches != null;
+            for(Map.Entry<String, String> matching : matches.entrySet()){
+                temp.append(matching.getKey()).append(" ").append(matching.getValue()).append('\n');
+            }
+        }
+
+        try{
+            File outFile = new File(output);
+            if(outFile.createNewFile()){
+                System.out.println("New File Created");
+            }
+
+            FileWriter writer = new FileWriter(output);
+            writer.write(String.valueOf(temp));
+            writer.close();
+
+        } catch(IOException e){
+            e.printStackTrace();
         }
     }
 
@@ -88,11 +127,28 @@ public class Stable {
                 }
                 else{
                     List<String> womenPref = womenRanking.get(woman);
-
+                    String other = getKey(matches, woman);
+                    if(womenPref.indexOf(current.replace("m", "")) < womenPref.indexOf(other.replace("m", ""))){
+                        matches.remove(other, woman);
+                        matches.put(current, woman);
+                        freeMen.add(other);
+                    }else{
+                        continue;
+                    }
                 }
+                break;
             }
         }
 
         return matches;
+    }
+
+    public String getKey(Map<String, String> map, String value){
+        for(Map.Entry<String, String> entry : map.entrySet()){
+            if(entry.getValue().equals(value)){
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 }
